d474e28acd2f56d088d806ff668638188e0be386
RachmanForniandi <rachmanforniandi@gmail.com>
2017-11-30 10:06:57 +0700

    display toy data list

--- app/src/main/java/com/example/android/simpletoyapp/MainActivity.java
+++ app/src/main/java/com/example/android/simpletoyapp/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.android.simpletoyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mToyListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToyListTextView = (TextView)findViewById(R.id.txtView_toy_names);
+
+        String[] toyNames = ToyBox.getToyNames();
+
+        for (String toyName : toyNames){
+            mToyListTextView.append(toyName + "\n\n\n");
+        }
     }
 }
